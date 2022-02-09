985221f3424b4fed3f8a721ebf049d113b1cda93
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-02-09 00:57:53 -0500

    Added stylesheets

--- wp-content/themes/mst/inc/block-editor.php
+++ wp-content/themes/mst/inc/block-editor.php
@@ -14,5 +14,12 @@ function mst_enqueue_block_editor_assets () {
         array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
         filemtime( get_template_directory().'/assets/js/block-editor.js')
     );
-}
+} 
 add_action( 'enqueue_block_editor_assets','mst_enqueue_block_editor_assets' );
+
+function mst_stylesheet() {
+    wp_enqueue_style(
+        'block-editor-style',
+        get_template_directory_uri().'/assets/css/block-editor.css');
+}
+add_action( 'enqueue_block_assets', 'mst_stylesheet' );
