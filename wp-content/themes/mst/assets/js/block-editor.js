bceb6f245faddb84ddeb5a6b76a90cce2b5f69d0
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-02-09 19:07:28 -0500

    Added custom font for paragraphs and headings

--- wp-content/themes/mst/assets/js/block-editor.js
+++ wp-content/themes/mst/assets/js/block-editor.js
@@ -6,3 +6,13 @@ wp.domReady( function () {
     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
 } );
+
+
+wp.blocks.registerBlockStyle( 'core/heading', {
+    name: 'mst-heading',
+    label: 'MST Heading',
+} );
+wp.blocks.registerBlockStyle( 'core/paragraph', {
+    name: 'mst-paragraph',
+    label: 'MST Paragraph',
+} );
