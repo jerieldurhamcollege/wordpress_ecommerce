07b6a97e4804a71b65330bb3e54aee484b7adef6
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-02-13 18:41:23 -0500

    Added FontAwesome

--- wp-content/themes/mst/functions.php
+++ wp-content/themes/mst/functions.php
@@ -142,6 +142,26 @@ function mst_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'mst_scripts' );
 
+/**
+ * Font Awesome Kit Setup
+ * This is my own fontawesome kit.
+ */
+if (! function_exists('fa_custom_setup_kit') ) {
+	function fa_custom_setup_kit($kit_url = '') {
+	    foreach ( [ 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts' ] as $action ) {
+		    add_action(
+		    $action,
+		    function () use ( $kit_url ) {
+			wp_enqueue_script( 'font-awesome-kit', $kit_url, [], null );
+	        }
+		);
+    }
+	}
+}
+
+fa_custom_setup_kit('https://kit.fontawesome.com/c0fa374032.js');
+
+
 
 /**
  * Custom template tags for this theme.
