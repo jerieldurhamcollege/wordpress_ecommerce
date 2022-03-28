b8f2c18909367b21a5351f5070076457c6e9f7c2
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2022-03-28 17:01:07 -0400

    Template & Archive

---
+++ themes/mst/single-auction.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying all auctions
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package ModernStoreTheme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+			?>
+			<div class="post-social-links">
+				<h3>Share this auction</h3>
+				<?php
+					mst_social_media_menu();
+				?>
+			</div>
+			<?
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'mst' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'mst' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// // If comments are open or we have at least one comment, load up the comment template.
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer();
